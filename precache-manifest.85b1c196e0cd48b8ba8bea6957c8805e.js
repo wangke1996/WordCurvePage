b52955e5920c528e0639a5b311109fe91a1c721e
wangke1996 <porkw@outlook.com>
2019-11-27 16:06:08 +0800

    Updates

---
+++ precache-manifest.85b1c196e0cd48b8ba8bea6957c8805e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5792d9eec415d39cbab05e5d2bcb3ee",
+    "url": "/WordCurvePage/index.html"
+  },
+  {
+    "revision": "620d895306152c1645fb",
+    "url": "/WordCurvePage/static/css/2.27f97842.chunk.css"
+  },
+  {
+    "revision": "620d895306152c1645fb",
+    "url": "/WordCurvePage/static/js/2.b0f7f434.chunk.js"
+  },
+  {
+    "revision": "063e6ddeb60c85d4900e",
+    "url": "/WordCurvePage/static/js/main.6f8a2aad.chunk.js"
+  },
+  {
+    "revision": "18a8f5c9e5c9bc2da725",
+    "url": "/WordCurvePage/static/js/runtime-main.de92ff6e.js"
+  }
+]);
